7e80f9efe9c831be20335d7dab6046fd54ba0847
dibademir <77748224+dibademir@users.noreply.github.com>
2021-11-10 15:02:20 +0300

    Initial Commit

---
+++ Memorize App/Memorize_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_AppApp.swift
+//  Memorize App
+//
+//  Created by Diba Demir on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
